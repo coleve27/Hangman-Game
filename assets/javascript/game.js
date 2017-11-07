7f0e7f83c65767a781d437afffac8b938f19164c
Evelyn Coleman <coleve27@gmail.com>
2017-11-06 20:34:43 -0600

    edited files

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+
+alert("yes");
+//press any key to start //
+var x = document.getElementById('element') .addEventListener("click", displayDate);
+
+
+
+
+//word is picked out of array and underscores correspond to letters//
+
+
+//user picks a letter and display is updated//
+
+
+//if user chooses correclty, letter populates in current word. Nothing is subtracted from guesses remaining, and letter does populate in letters guessed//
+
+
+//if user chooses incorrectly, 1 is subtracted from guesses remaining and letter populates in letters guessed//
+
+
+//if guesses remaining is >0 loop back to next round//
+
+//if all letters are populated then add 1 to win and begin new game//
+
+//if guesses = 0 begin a new game//
