ecf35bcdf8e310361262bc3387b71810ad82a051
Evelyn Coleman <coleve27@gmail.com>
2017-11-07 18:18:51 -0600

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,39 @@
-
 alert("yes");
+
 //press any key to start //
-var x = document.getElementById('element') .addEventListener("click", displayDate);
+
+document.onkeyup = function (e) {
+  console.log("anything");
+}
+// var x = document.getElementById('element') .addEventListener("click", displayDate);
 
 
 
 
 //word is picked out of array and underscores correspond to letters//
 
+var mysteryWord = [
+  "subaru",
+  "volvo",
+  "Toyota"
+]
+
+
+// for loop pick words out of wordbank <-- Crap
+//math random multiply by words in the array//
+// split the string into array
+//
+// for loop, inside of a function (replaceUnderscore?),
+//
+// if index( true ) then replace with "_"
+
 
 //user picks a letter and display is updated//
+
+
+
+
+
 
 
 //if user chooses correclty, letter populates in current word. Nothing is subtracted from guesses remaining, and letter does populate in letters guessed//
